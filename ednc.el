sinic/ednc

commit e19a053ac046f17d40337fb5d2b3b12e135ab110

Create image from path only if data could be read

ednc.el
@@ -267,7 +267,7 @@ The returned value is removed from HINTS if REMOVE-FLAG is non-nil."
         (with-temp-buffer
           (set-buffer-multibyte nil)
           (ignore-errors (insert-file-contents-literally image-path))
-          (create-image (buffer-string) nil t))
+          (when (> (point) 1) (create-image (buffer-string) nil t)))
       (throw 'invalid (message "unsupported image path: %s" image-path)))))
 
 (defun ednc--data-to-image (image-data)

commit 81598be8c6cd658c3e93e1a1535707b8c8b63ac9

Fix regression introduced with commit e19a053
The function insert-file-contents-literally does not move point, as
opposed to the function insert. Instead of looking at point, check
whether the buffer is empty.

ednc.el
@@ -267,7 +267,8 @@ The returned value is removed from HINTS if REMOVE-FLAG is non-nil."
         (with-temp-buffer
           (set-buffer-multibyte nil)
           (ignore-errors (insert-file-contents-literally image-path))
-          (when (> (point) 1) (create-image (buffer-string) nil t)))
+          (unless (string-empty-p (buffer-string))
+            (create-image (buffer-string) nil t)))
       (throw 'invalid (message "unsupported image path: %s" image-path)))))
 
 (defun ednc--data-to-image (image-data)